d3c5c51659098e5f31b85d05481d9bb1bbfb7852
Paul Richardson <paul.richardson@ellucian.com>
2016-04-08 13:06:18 -0600

    Make the 2 dictionary types consistent

--- src/words/core.cljs
+++ src/words/core.cljs
@@ -13,17 +13,17 @@
 (let [c (chan)
       m (mult c)
       buckets (chan)]
-  (def raw-chan (chan))
+  (def ^:private raw-chan (chan))
   (tap m raw-chan)
   (tap m buckets)
-  (defonce dict (atom {}))
+  (defonce ^:private dict (atom {}))
   (go (prepare-buckets (<! buckets)))
   (read-dict #(go (->> %2
                        clojure.string/split-lines
                        (map (fn [w] (.toLowerCase w)))
                        (>! c)))))
 
-(defn prepare-dictionary [d]
+(defn- prepare-dictionary [d]
   (-> (for [i (range 1 (count (first d)))]
          (loop [sub-d d
                 words []]
@@ -34,21 +34,15 @@
       vec
       (conj (set d))))
 
-(defn prepare-buckets [raw]
+(defn- prepare-buckets [raw]
   (when (-> (count @dict) (= 0))
     (doseq [l (range 2 25)] ;; highest in dict
       (swap! dict assoc l (prepare-dictionary (filter #(= l (count %)) raw))))))
 
-(defn index->coords [i w] [(rem i w) (int (/ i w))])
-(defn coords->index [x y w] (+ x (* w y)))
+(defn- index->coords [i w] [(rem i w) (int (/ i w))])
+(defn- coords->index [x y w] (+ x (* w y)))
 
-(defn maybe-word?
-  [d word]
-  (let [possible-word (->> d (drop-while #(-> % (compare word) (< 0))) first)]
-    (when possible-word
-      (clojure.string/starts-with? possible-word word))))
-
-(defn adjacent
+(defn- adjacent
   [i w h]
   (let [[x y] (index->coords i w)
         delta (range -1 2)]
@@ -60,11 +54,13 @@
       (coords->index new-x new-y w))))
 
 ;; board: [w h [...]] where the vector acts as an associative for index->letter
-(defn next-indices
+(defn- next-indices
   "word is the partial
-  i is the index of the last letter of word
-  ->letter is the vector of letters
-  note: i can be mapped to nil or not, it won't be used"
+  i is the index (into the board) of the last letter of word
+  ->letter is the vector of letters (the \"board\")
+  in-dict? is a fn which will receive a word and say whether it's valid or to abort/prune
+  note: i can be mapped to nil on the board, or a letter. Since it's not adjacent to itself,
+     it won't matter. Previously used letters should be nil"
   [i word [w h ->letter] in-dict?]
   (->> (adjacent i w h)
        (filter #(->letter %))
@@ -72,23 +68,45 @@
                      (str word)
                      in-dict?))))
 
-(defn use-index [i board]
+(defn- use-index [i board]
   (assoc-in board [2 i] nil))
 
-(defn find-words [len word indices board d]
+(defn- find-words
+  "d should be a dictionary *function*
+  pass it a word to get a new fn f:
+  invoke f with no args for the new dict to pass to the recursive call
+  invoke f with one arg (a word) to use as a pred - use-word?"
+  [len word indices board d]
   (let [index->word #(str word (get-in board [2 %]))]
     (if (= len 1)
       (map index->word indices)
       (mapcat
        #(let [new-word (index->word %)
-              [new-dict use-word?]
-              (if (vector? d)
-                [d (-> new-word count inc d)]
-                [(drop-while (fn [e] (-> e (compare new-word) (< 0))) d)
-                 (partial maybe-word? new-dict)])
-              new-indices (next-indices % new-word board use-word?)]
-          (find-words (dec len) new-word new-indices (use-index % board) new-dict))
+              new-dict (d new-word)
+              new-indices (next-indices % new-word board new-dict)]
+          (find-words (dec len) new-word new-indices (use-index % board) (new-dict)))
        indices))))
+
+(defn- maybe-word?
+  [d word]
+  (let [possible-word (->> d (drop-while #(-> % (compare word) (< 0))) first)]
+    (when possible-word
+      (clojure.string/starts-with? possible-word word))))
+
+(defn- new-d [d]
+  (fn [w]
+    (let [nd (drop-while #(-> % (compare w) (< 0)) d)]
+         (fn
+           ([] (new-d nd))
+           ([nw] (maybe-word? nd nw))))))
+
+(defn- dict-fn [l]
+  (or (when-let [d (@dict l)]
+        (fn this [_]
+          (fn
+            ([] this)
+            ([w] ((-> w count d) w)))))
+      (new-d (filter #(= l (count %)) raw-dict))))
 
 (defn solve [board w h l]
   (let [letters (vec (replace {\- nil} board))]
@@ -97,15 +115,13 @@
                                range
                                (filter letters))
                      [w h letters]
-                     (or (and (contains? @dict l)
-                              (@dict l))
-                         (filter #(= l (count %)) raw-dict)))
+                     (dict-fn l))
          set
          sort)))
 
 (defn -main [board & lwh]
   (when board
-    (go (defonce raw-dict (<! raw-chan))
+    (go (defonce ^:private raw-dict (<! raw-chan))
         (let [[l w h]
               (map int
                    (if (< (count lwh) 3)
