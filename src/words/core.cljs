fd2253f0ebfc1ad08731fc9b25144ee260a607fb
Paul Richardson <paul.richardson@ellucian.com>
2016-11-03 13:15:25 -0600

    Changes from time at Ellucian. No idea what any more

--- src/words/core.cljs
+++ src/words/core.cljs
@@ -42,50 +42,31 @@
 (defn- index->coords [i w] [(rem i w) (int (/ i w))])
 (defn- coords->index [x y w] (+ x (* w y)))
 
-(defn- adjacent
-  [i w h]
-  (let [[x y] (index->coords i w)
-        delta (range -1 2)]
-    (for [new-y (map #(+ y %) delta)
-          :when (< -1 new-y h)
-          new-x (map #(+ x %) delta)
-          :when (< -1 new-x w)
-          :when (or (not= x new-x) (not= y new-y))]
-      (coords->index new-x new-y w))))
-
-;; board: [w h [...]] where the vector acts as an associative for index->letter
-(defn- next-indices
-  "word is the partial
-  i is the index (into the board) of the last letter of word
-  ->letter is the vector of letters (the \"board\")
-  in-dict? is a fn which will receive a word and say whether it's valid or to abort/prune
-  note: i can be mapped to nil on the board, or a letter. Since it's not adjacent to itself,
-     it won't matter. Previously used letters should be nil"
-  [i word [w h ->letter] in-dict?]
-  (->> (adjacent i w h)
-       (filter #(->letter %))
-       (filter #(->> (->letter %)
-                     (str word)
-                     in-dict?))))
+(def adjacent (memoize (fn[i w h]
+                   (let [[x y] (index->coords i w)
+                         delta (range -1 2)]
+                     (for [new-y (map #(+ y %) delta)
+                           :when (< -1 new-y h)
+                           new-x (map #(+ x %) delta)
+                           :when (< -1 new-x w)
+                           :when (or (not= x new-x) (not= y new-y))]
+                       (coords->index new-x new-y w))))))
 
 (defn- use-index [i board]
   (assoc-in board [2 i] nil))
 
-(defn- find-words
-  "d should be a dictionary *function*
-  pass it a word to get a new fn f:
-  invoke f with no args for the new dict to pass to the recursive call
-  invoke f with one arg (a word) to use as a pred - use-word?"
-  [len word indices board d]
-  (let [index->word #(str word (get-in board [2 %]))]
-    (if (= len 1)
-      (map index->word indices)
-      (mapcat
-       #(let [new-word (index->word %)
-              new-dict (d new-word)
-              new-indices (next-indices % new-word board new-dict)]
-          (find-words (dec len) new-word new-indices (use-index % board) (new-dict)))
-       indices))))
+(def find-words
+  "l is the length of word to find
+  s is the board size
+  i is the index to search from"
+  (memoize
+   (fn [l w h i]
+     ;; find all adjacent indices, and find all words of n-1 from those. append index to each
+     (if (= l 1)
+       [(list i)]
+       (->> (adjacent i w h)
+            (mapcat #(find-words (dec l) w h %))
+            (map #(conj % i)))))))
 
 (defn- new-d [d]
   (fn [w]
@@ -107,14 +88,16 @@
 (defn solve
   ([board w h l optimized]
    (let [letters (vec (replace {\- nil} board))]
-     (->> (find-words l "" (->> board
-                                count
-                                range
-                                (filter letters))
-                      [w h letters]
-                      (dict-fn l optimized))
+     (->> board
+          count
+          range
+;;          (filter letters) ; ?
+          (mapcat #(find-words l w h %))
+;;          (dict-fn l optimized)
           set
-          sort)))
+          count
+          ;; sort
+          )))
   ([b l]
    (let [s (.sqrt js/Math (count b))]
      (solve b s s l)))
